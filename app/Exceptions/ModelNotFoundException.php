10043a53e933982db509cb4500e73a310ee276f1
jordiForn <thejfk18@gmail.com>
2025-05-15 12:36:56 +0200

    Better ModelException

--- app/Exceptions/ModelNotFoundException.php
+++ app/Exceptions/ModelNotFoundException.php
@@ -1,5 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Exceptions;
 
-class ModelNotFoundException extends \Exception{}
+class ModelNotFoundException extends \RuntimeException
+{
+    public function __construct(string $message = "Model not found", int $code = 404, \Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
